ec595d2a9096104310a83b1851fe2441fdbd104a
Sudarsh Nanjappa <sudarsh.nanjappa@wmich.edu>
2015-07-05 11:21:11 -0400

    offlining for apps
    offline

---
+++ www/js/index.js
@@ -0,0 +1,62 @@
+
+var app = {
+    // Application Constructor
+	internetconnection :null,
+	
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+    	 //document.addEventListener("online", onOnline, false);
+    	//document.addEventListener("offline", onOffline, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        
+    },
+   
+    
+    
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event d: ' + id);
+        checkConnection();
+    } 
+};
+ 
+function checkConnection() { 
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+   
+    
+    app.internetconnection = states[networkState];
+    alert('Connection type: ' + app.internetconnection);
+}
+
+app.initialize();
